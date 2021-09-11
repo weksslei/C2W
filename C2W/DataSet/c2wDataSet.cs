3907998e6c6ff03f1bdb056b74c53e64832618e3
Weksslei <weksslei@live.com>
2021-09-11 13:59:30 -0300

    Removed some files

---
+++ C2W/DataSet/c2wDataSet.cs
@@ -0,0 +1,19 @@
+﻿namespace C2W.DataSet
+{
+
+
+    partial class c2wDataSet
+    {
+    }
+}
+
+namespace C2W.DataSet.c2wDataSetTableAdapters
+{
+    partial class produtosTableAdapter
+    {
+    }
+
+    public partial class portifolioTableAdapter
+    {
+    }
+}
